e5649e5197849d95f9e95e4095bf6817886e35ae
tjbass2021 <tjbass2021@gmail.com>
2022-05-29 14:55:52 -0300

    feat: implementação de menu vertical

--- scripts/menu.js
+++ scripts/menu.js
@@ -3,13 +3,19 @@ $(document).ready(function(){
     let controle = false
 
     $('.menu-hamburguer').on('click', function(){
-        if(controle === false){
-            $('.menu-oculto').slideDown();
+        if(! controle){
             $('.menu-oculto').addClass('visivel');
+            $('.exit').addClass('exit-vivsivel');
+            $(this).addClass('menu-hamburguer-none');
             controle = true;
-        }else{
-            $('.menu-oculto').slideUp();
+        }
+    })
+
+    $('.exit').on('click', function(){
+        if(controle) {
             $('.menu-oculto').removeClass('visivel');
+            $('.menu-hamburguer').removeClass('menu-hamburguer-none');
+            $(this).removeClass('exit-vivsivel');
             controle = false;
         }
     })
